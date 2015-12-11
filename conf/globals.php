bfdd5a9d8c97a3a04eb7810fa6bd454da0aba9ae
Andrey Shmits <a.shmitz@mtrx.hk>
2015-12-11 20:23:33 +0300

    add site

---
+++ conf/globals.php
@@ -0,0 +1,14 @@
+<?php
+
+  define("DB_HOST","localhost");
+  define("DB_NAME","sqc");
+  define("DB_USER","root");
+  define("DB_PASS","password");
+
+  define("ROOT", dirname(__FILE__) . "/../");
+  define("DOMAIN", 'http://'.$_SERVER['SERVER_NAME'].'/');
+  define("SHOP_IMAGE", DOMAIN . 'uploaded_files/shop_images/');
+  define("TPL_PATH", ROOT . "templates/");
+  define("TPL_SHOP_PATH", ROOT . "modules/shop/front/templates/");
+  
+?>
